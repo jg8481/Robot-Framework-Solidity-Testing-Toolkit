6ca49497b2ddb5812a2573c12514d11896fcf601
jg8481 <joshua.gorospe@gmail.com>
2023-02-12 23:32:36 -0500

    Fix the mistakes in the start-maintenance-workflows.sh and start-solidity-qa-workflows.sh files

--- start-solidity-qa-workflows.sh
+++ start-solidity-qa-workflows.sh
@@ -15,9 +15,8 @@ if [ "$1" == "Stop-All-Local-Network-Nodes-And-Delete-Logs-On-MacOS-Or-Linux" ];
   pkill node > /dev/null 2>&1
   pkill npm > /dev/null 2>&1
   pkill robot > /dev/null 2>&1
-  cd ./solidity-hardhat-multichain-tools/logs
-  ls -la
-  rm -rf ./*.log
+  rm -rf ./solidity-hardhat-multichain-tools/logs/*.log
+  rm -rf ./solidity-truffle-web3js-tools/logs/*.log
   TIMESTAMP2=$(date)
   echo "This run ended on $TIMESTAMP2."
 fi
@@ -40,6 +39,9 @@ if [ "$1" == "Install-Tools-On-MacOS-Or-Linux" ]; then
   npm install -g surya
   npm install --save-dev hardhat
   npm install --save-dev @nomicfoundation/hardhat-toolbox
+  npm install -g truffle
+  npm install @truffle/contract
+  npm install web3
   npm install robotremote
   npm update
   npm audit fix --force
@@ -54,7 +56,7 @@ if [ "$1" == "Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-O
   echo
   echo "This command was designed to run a built-in Hardhat Network locally running Ethereum node in its own terminal window. It will also start the node-robotremoteserver. This run started on $TIMESTAMP."
   echo
-  echo "Please read this documentation to learn more about the Hardhat Network... https://hardhat.org/hardhat-network/docs/overview"
+  echo "Please read this documentation to learn more about the Hardhat Network tool... https://hardhat.org/hardhat-network/docs/overview"
   echo
   killall node > /dev/null 2>&1
   killall npm > /dev/null 2>&1
@@ -105,7 +107,7 @@ if [ "$1" == "Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacO
   echo
   echo "This command was designed to run a forked EVM compatible blockchain mainnet on Hardhat Network using a specific configuration in its own terminal window. Ethereum, Fantom, Avalanche and Polygon are included, but many more are possible. It will also start the node-robotremoteserver. This run started on $TIMESTAMP."
   echo
-  echo "Please read this documentation to learn more about the Hardhat Network... https://hardhat.org/hardhat-network/docs/overview"
+  echo "Please read this documentation to learn more about the Hardhat Network tool... https://hardhat.org/hardhat-network/docs/overview"
   echo
   killall node > /dev/null 2>&1
   killall npm > /dev/null 2>&1
@@ -170,7 +172,7 @@ if [ "$1" == "Run-Smart-Contract-Interactive-Tests-On-Local-NonForked-Hardhat-Ne
     echo
   else 
     echo
-    echo "BLOCKCHAIN_TYPE_FILE is not set. Please run the 'Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command or 'Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command, follow the instructions, and wait until you see a 'Block #1:' appear in the terminal window. This test is going to exit now."
+    echo "BLOCKCHAIN_TYPE_FILE is not set. Please run the 'Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command or 'Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command, follow the instructions, and wait until you see 'Block #1:' appear in the terminal window. This test is going to exit now."
     echo
     exit
   fi
@@ -204,7 +206,7 @@ if [ "$1" == "Run-Smart-Contract-Interactive-Tests-On-Forked-Mainnet-Hardhat-Net
     echo
   else 
     echo
-    echo "BLOCKCHAIN_TYPE_FILE is not set. Please run the 'Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command or 'Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command, follow the instructions, and wait until you see a 'Block #1:' appear in the terminal window. This test is going to exit now."
+    echo "BLOCKCHAIN_TYPE_FILE is not set. Please run the 'Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command or 'Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacOS-Or-Linux' command, follow the instructions, and wait until you see 'Block #1:' appear in the terminal window. This test is going to exit now."
     echo
     exit
   fi
@@ -335,6 +337,99 @@ if [ "$1" == "Run-Smart-Contract-Hardhat-Avalanche-RPA-Deployment" ]; then
   exit
 fi
 
+if [ "$1" == "Start-Default-Truffle-Develop-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux" ]; then
+  echo
+  echo "------------------------------------[[[[ Start-Default-Truffle-Develop-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux ]]]]------------------------------------"
+  echo
+  echo "This command was designed to run a built-in Truffle Develop locally running Ethereum node in its own terminal window. It will also start the node-robotremoteserver. This run started on $TIMESTAMP."
+  echo
+  echo "Please read this documentation to learn more about the Truffle Develop tool... https://trufflesuite.com/docs/truffle/how-to/debug-test/use-truffle-develop-and-the-console/"
+  echo
+  killall node > /dev/null 2>&1
+  killall npm > /dev/null 2>&1
+  killall robot > /dev/null 2>&1
+  pkill node > /dev/null 2>&1
+  pkill npm > /dev/null 2>&1
+  pkill robot > /dev/null 2>&1
+  cd ./solidity-truffle-web3js-tools
+  rm -rf ./logs/truffle-default-network-node-standalone-mode.log
+  rm -rf ./logs/robotframework-truffle-remote-library-standalone-mode.log
+  rm -rf ./logs/truffle-contract-compile.log
+  echo
+  echo
+  truffle compile > ./logs/truffle-contract-compile.log &&
+  echo
+  echo
+  truffle develop > ./logs/truffle-default-network-node-standalone-mode.log &
+  sleep 10
+  echo
+  echo
+  truffle migrate | grep "contract address:" | awk '{print $4}' > ./logs/truffle-contract-address.log &&
+  echo
+  echo
+  nohup node ./robotframework-truffle-remote-library.js > ./logs/robotframework-truffle-remote-library-standalone-mode.log &
+  sleep 10
+  TIMESTAMP2=$(date)
+  echo "------------------------------------[[[[ Truffle-Develop-Ethereum-Node-And-Robotemoteserver-Log-Output ]]]]------------------------------------"
+  echo
+  echo "************************************"
+  echo "The following log output comes from the truffle-contract-address.log"
+  cat ./logs/truffle-contract-address.log 
+  echo
+  echo "************************************"
+  echo "The following log output comes from the robotframework-truffle-remote-library-standalone-mode.log"
+  cat ./logs/robotframework-truffle-remote-library-standalone-mode.log
+  echo
+  echo "************************************"
+  echo "The following log output comes from the truffle-default-network-node-standalone-mode.log"
+  tail -F ./logs/truffle-default-network-node-standalone-mode.log
+  echo
+  echo "This run ended on $TIMESTAMP2."
+fi
+
+if [ "$1" == "Run-Smart-Contract-Interactive-Tests-On-Local-Ethereum-Truffle-Develop-Instance" ]; then
+  clear
+  echo
+  echo "------------------------------------[[[[ Run-Smart-Contract-Interactive-Tests-On-Local-Ethereum-Truffle-Develop-Instance ]]]]------------------------------------"
+  echo
+  echo "This command will run Robot Framework Javascript automation that will perform an interactive test on Solidity Smart Contracts using the built-in Truffle Develop blockchain designed for local development. This run started on $TIMESTAMP."
+  echo
+  cd ./solidity-truffle-web3js-tools
+  export CONTRACT_ADDRESS=$(cat ./logs/truffle-contract-address.log)
+  pip3 install virtualenv --user > /dev/null 2>&1
+  virtualenv -p python3 venv > /dev/null 2>&1
+  source venv/bin/activate
+  pip3 install -r ./requirements.txt > /dev/null 2>&1
+  sleep 5
+  robot --report NONE --log smart-contract-truffle-tests-log.html --output smart-contract-truffle-tests-output.xml -N "Robot Framework Solidity Smart Contract Truffle Ethereum Test" -d ./logs ./robotframework-truffle-test-keywords.robot
+  TIMESTAMP2=$(date)
+  echo "This run ended on $TIMESTAMP2."
+  exit
+fi
+
+if [ "$1" == "Run-Smart-Contract-Truffle-Ethereum-RPA-Deployment" ]; then
+  clear
+  killall node > /dev/null 2>&1
+  killall npm > /dev/null 2>&1
+  killall robot > /dev/null 2>&1
+  pkill node > /dev/null 2>&1
+  pkill npm > /dev/null 2>&1
+  pkill robot > /dev/null 2>&1
+  echo
+  echo "------------------------------------[[[[ Run-Smart-Contract-Truffle-Ethereum-RPA-Deployment ]]]]------------------------------------"
+  echo
+  echo "This command will run Robot Framework RPA automation that compiles and deploys Solidity Smart Contracts using the built-in Truffle Develop designed for local development. This run started on $TIMESTAMP."
+  echo
+  pip3 install virtualenv --user > /dev/null 2>&1
+  virtualenv -p python3 venv > /dev/null 2>&1
+  source venv/bin/activate
+  pip3 install -r ./solidity-truffle-web3js-tools/requirements.txt > /dev/null 2>&1
+  robot --variable BLOCKCHAIN:"Ethereum" --report NONE --log smart-contract-truffle-deployment-log.html --output smart-contract-truffle-deployment-output.xml -N "Robot Framework Solidity Smart Contract Truffle Deployment Ethereum" -d ./solidity-truffle-web3js-tools/logs ./solidity-truffle-web3js-tools/robotframework-truffle-deployment-rpa-keywords.robot
+  TIMESTAMP2=$(date)
+  echo "This run ended on $TIMESTAMP2."
+  exit
+fi
+
 usage_explanation() {
   echo
   echo
@@ -349,6 +444,7 @@ usage_explanation() {
   echo "bash ./start-solidity-qa-workflows.sh Install-Tools-On-MacOS-Or-Linux"
   echo "bash ./start-solidity-qa-workflows.sh Start-Default-Hardhat-Network-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux"
   echo "bash ./start-solidity-qa-workflows.sh Start-Multichain-Hardhat-Network-Node-And-Robotemoteserver-On-MacOS-Or-Linux"
+  echo "bash ./start-solidity-qa-workflows.sh Start-Default-Truffle-Develop-Ethereum-Node-And-Robotemoteserver-On-MacOS-Or-Linux"
   echo "bash ./start-solidity-qa-workflows.sh Stop-All-Local-Network-Nodes-And-Delete-Logs-On-MacOS-Or-Linux"
   echo 
   echo "---->>>> Solidity Static Analysis And Security Testing Commands <<<<----"
@@ -360,9 +456,15 @@ usage_explanation() {
   echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Hardhat-Polygon-RPA-Deployment"
   echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Hardhat-Avalanche-RPA-Deployment"
   echo
+  echo "---->>>> Truffle Deployment Commands Powered by Robot Framework RPA (NOTE: These scripts are capable of deploying to Mainnets or Testnets!) <<<<----"
+  echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Truffle-Ethereum-RPA-Deployment"
+  echo
   echo "---->>>> Interactive Tests For Deployed Smart Contracts Using Hardhat (NOTE: These tests can run on multiple EVM Compatible Blockchains!) <<<<----"
   echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Interactive-Tests-On-Local-NonForked-Hardhat-Network"
   echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Interactive-Tests-On-Forked-Mainnet-Hardhat-Network" 
+  echo
+  echo "---->>>> Interactive Tests For Deployed Smart Contracts Using Truffle Suite <<<<----"
+  echo "bash ./start-solidity-qa-workflows.sh Run-Smart-Contract-Interactive-Tests-On-Local-Ethereum-Truffle-Develop-Instance"
   echo
   echo
   cat ./notes-images-and-demonstrations/NOTES.md 2> /dev/null
